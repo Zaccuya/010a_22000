29b00bbe8d81e63bdab180e5932a2248ab8abdce
Zaccuya <125554690+Zaccuya@users.noreply.github.com>
2023-06-21 13:36:12 +0900

    加速度Xブロックを使用した傾きを計算

--- main.ts
+++ main.ts
@@ -1,54 +1,12 @@
-let number = 0
-input.onGesture(Gesture.Shake, function () {
-    basic.clearScreen()
-    number = randint(1, 6)
-    if (number == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    } else if (number == 2) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # . . . #
-            . . . . .
-            . . . . .
-            `)
-    } else if (number == 3) {
-        basic.showLeds(`
-            . . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . .
-            `)
-    } else if (number == 4) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            `)
-    } else if (number == 5) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . #
-            `)
-    } else {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . . . #
-            `)
+let y = 0
+let x = 2
+basic.forever(function () {
+    y = 0
+    x = (-1 - input.acceleration(Dimension.X) + 1000) / 400
+    for (let index = 0; index < 5; index++) {
+        led.plot(x, y)
+        y += 1
     }
+    basic.pause(100)
+    basic.clearScreen()
 })
